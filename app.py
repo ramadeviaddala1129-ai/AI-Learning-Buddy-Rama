716b6205bd332772d817db1840b2b2c1b783e242
ramadeviaddala1129-ai <ramadeviaddala1129@gmail.com>
2026-07-10 18:27:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 import streamlit as st
-import google.generativeai as genai
-# Configure Gemini API
-genai.configure(api_key=st.secrets["GEMINI_API_KEY"])
-model = genai.GenerativeModel("models/gemini-2.5-flash")
+from google import genai
+
+# Configure Gemini Client
+client = genai.Client(api_key=st.secrets["GEMINI_API_KEY"])
+
 st.set_page_config(page_title="AI Learning Buddy Rama", page_icon="🎓")
+
 st.title("🎓 AI Learning Buddy Rama")
+
 topic = st.text_input("Enter a Topic")
+
 option = st.selectbox(
     "Choose Activity",
-     [
+    [
         "Explain Concept",
         "Real-Life Example",
         "Generate Quiz",
@@ -18,24 +22,26 @@ option = st.selectbox(
 
 if st.button("Generate"):
 
-    if topic == "":
-      st.warning("Please enter a topic.")
+    if topic.strip() == "":
+        st.warning("Please enter a topic.")
 
     else:
-      if option == "Explain Concept":
-          prompt = f"Explain {topic} in simple language for a beginner."
 
-      elif option == "Real-Life Example":
-           prompt = f"Give one simple real-life example of {topic}."
+        if option == "Explain Concept":
+            prompt = f"Explain {topic} in simple language for a beginner."
 
-      elif option == "Generate Quiz":
-        prompt = f"Create 5 MCQs on {topic} with answers."
+        elif option == "Real-Life Example":
+            prompt = f"Give one simple real-life example of {topic}."
 
+        elif option == "Generate Quiz":
+            prompt = f"Create 5 multiple choice questions on {topic} with answers."
 
+        else:
+            prompt = topic
 
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt
+        )
 
-
-      else:
-           prompt = topic
-      response = model.generate_content(prompt)
-      st.write(response.text)
+        st.write(response.text)
